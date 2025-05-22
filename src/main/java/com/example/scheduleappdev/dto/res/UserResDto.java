ca3e78db87883bad12386413383fd8ca24851e30
ryusu12 <rsy7567@gmail.com>
2025-05-22 17:59:13 +0900

    :recycle: refactor: 유저 정보 리턴값에 비밀번호 제외
    UserResDto password 필드 삭제

--- src/main/java/com/example/scheduleappdev/dto/res/UserResDto.java
+++ src/main/java/com/example/scheduleappdev/dto/res/UserResDto.java
@@ -11,7 +11,6 @@ public class UserResDto {
     private final Long userId;
     private final String userName;
     private final String userEmail;
-    private final String password;
     private final LocalDateTime createdDateTime;
     private final LocalDateTime updatedDateTime;
 
@@ -19,7 +18,6 @@ public class UserResDto {
         this.userId = user.getId();
         this.userName = user.getName();
         this.userEmail = user.getEmail();
-        this.password = user.getPassword();
         this.createdDateTime = user.getCreatedDateTime();
         this.updatedDateTime = user.getUpdatedDateTime();
     }
